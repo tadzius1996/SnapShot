3d01adb669d6392a2ba2966445a2a35dbbd51c09
Tadas <milonciustadas@gmail.com>
2022-12-12 08:13:29 -0800

    save files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'scontent.fhan4-3.fna.fbcdn.net'],
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
+import React, {useEffect, useState} from 'react'
 import type { AppProps } from 'next/app'
+import SideBar from '../components/SideBar'
+import MyProfile from '../components/MyProfile'
+import {GoogleOAuthProvider} from '@react-oauth/google'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);  
+
+  if (isSSR) return null;
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+    <div className='flex overflow-x-hidden'>
+        <div className='    w-full'>
+          <SideBar />
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </div> 
+    </div>
+    </GoogleOAuthProvider>
+  )
 }
